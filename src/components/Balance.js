7cbcb7a71f84271862a063e22b92de0261ce1585
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2024-05-07 20:44:25 +0530

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,6 @@
-// use rafce
-
 import React from 'react' 
 import { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'  // here we are using double dot(..) instead of single dot(.)
-                                                        // maybe because both components and context are in the same folder 
-                                                        // of 'src'.
+import { GlobalContext } from '../context/GlobalState'  
 
 const Balance = () => {
 
@@ -13,9 +9,7 @@ const Balance = () => {
   const amounts = transactions.map((transaction) => transaction.amount)
 
   const total = amounts.reduce((total, num) => (total += num), 0).toFixed(2) 
-                                      // read reduce() from:  
-                                      // **1.> https://www.freecodecamp.org/news/how-to-use-javascript-array-reduce-method/
-                                      //   2.> https://www.w3schools.com/jsref/jsref_reduce.asp
+                                      
   return (
     <>
      <h4> Your Balance </h4>
